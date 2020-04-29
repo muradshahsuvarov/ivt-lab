f97ba6233864ad6162cc7935c23335cca18c711f
muradshahsuvarov <42478293+muradshahsuvarov@users.noreply.github.com>
2020-04-29 04:05:20 +0200

    update 2

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -30,7 +30,8 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
+    
     }
 
     boolean success = false;
